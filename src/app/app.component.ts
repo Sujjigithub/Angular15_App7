6a1e90e2a7aaa02ab1c79b45aa18abf03d471eea
Sujjigithub <jangilisudhakar7@gmail.com>
2024-06-09 21:26:38 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+
+import { createInjectableType } from '@angular/compiler';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app7';
+  msg: string = "Welcome to Angular";
+  constructor(@Inject(HttpClient) private httpClient: HttpClient) { }
+
+  getWelcomeMessage() {
+    // this.msg="Button clicked";
+    this.httpClient.get("http://localhost:8080/welcome", { responseType: 'text' })
+      .subscribe(response => {
+        this.msg = response;
+      });
+
+  }
+  getWishMessage() {
+    this.httpClient.get("http://localhost:8080/wish", { responseType: 'text' })
+      .subscribe(response => {
+        this.msg = response;
+      });
+  }
 }
+
+
